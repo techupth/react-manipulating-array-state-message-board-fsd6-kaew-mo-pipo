51db397611d12d0847d10a5a846b629c548eb08f
PiPo <pipo.khomgrid@gmail.com>
2024-06-06 17:11:07 +0700

    added stage list data

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messageInput, setMessageInput] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  function handleMessageInput(event) {
+    setMessageInput(event.target.value);
+  }
+  function addMessage(event) {
+    event.preventDefault();
+    setMessageList([...messageList, messageInput]);
+    // const newMessageList = [...messageList];
+    // newMessageList.push(messageInput);
+    // setMessageList(newMessageList);
+    setMessageInput("");
+  }
+
+  function removeMessage(index) {
+    const newMessageList = [...messageList];
+    newMessageList.splice(index, 1);
+    setMessageList(newMessageList);
+  }
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,15 +32,30 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={messageInput}
+            onChange={handleMessageInput}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addMessage}>
+          Submit
+        </button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {messageList.map((item, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1>{item}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  removeMessage(index);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
