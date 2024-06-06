c3fa44d00c410a59e091bcc61ff3382d890f92c4
Chavanvit <chavanvit.n@gmail.com>
2024-06-06 13:36:22 +0700

    feat: Add array state add&delete message -react-

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [todoMessage, setTodoMessage] = useState("");
+  const [keepMessage, setKeepMessage] = useState([]);
+
+  const changeTodoMessage = (event) => {
+    setTodoMessage(event.target.value);
+  };
+
+  const submitKeepMessage = () => {
+    const newKeepMessage = [...keepMessage];
+    newKeepMessage.push(todoMessage);
+    setKeepMessage(newKeepMessage);
+    setTodoMessage("");
+  };
+
+  const deleteMessage = (indexMessage) => {
+    const newKeepMessage = [...keepMessage];
+    newKeepMessage.splice(indexMessage, 1);
+    setKeepMessage(newKeepMessage);
+  };
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,15 +31,30 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={todoMessage}
+            onChange={changeTodoMessage}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={submitKeepMessage}>
+          Submit
+        </button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {keepMessage.map((Message, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1>{Message}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  deleteMessage(index);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
